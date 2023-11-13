34e3d3938f35e3721b08e9bb2eb786625dc65eec
MouhebSliti <mouheb.sliti@esprit.tn>
2023-11-13 09:43:45 +0100

    Updated API

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const https = require("https");
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.get('/processJson', (req, res) => {
-  const mobilePackage = req.body;
+let Jsondata;  // Declare Jsondata outside the callback to make it accessible
 
+fs.readFile("./MP1.json", "utf8", (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  Jsondata = JSON.parse(data);
+});
+
+app.get('/processJson', (req, res) => {
   const result = {
-    id: mobilePackage.id,
-    name: mobilePackage.name,
-    bundledProductOffering: mobilePackage.bundledProductOffering.map(product => {
+    id: Jsondata.id,
+    name: Jsondata.name,
+    bundledProductOffering: Jsondata.bundledProductOffering.map(product => {
       return {
         id: product.id,
         name: product.name
