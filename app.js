5f78c785fa6c260dc87e404852d0b85da4bcef30
MouhebSliti <mouheb.sliti@esprit.tn>
2023-11-23 17:06:58 +0100

    DISCO META

--- app.js
+++ app.js
@@ -2,36 +2,50 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const https = require("https");
-const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-let Jsondata;  // Declare Jsondata outside the callback to make it accessible
-
-fs.readFile("./MP1.json", "utf8", (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  Jsondata = JSON.parse(data);
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
-app.get('/processJson', (req, res) => {
-  const result = {
-    id: Jsondata.id,
-    name: Jsondata.name,
-    bundledProductOffering: Jsondata.bundledProductOffering.map(product => {
-      return {
-        id: product.id,
-        name: product.name
-      };
-    })
-  };
+async function fetchJsonDataFromApi() {
+  try {
+    const response = await axiosInstance.get(
+      "https://catalog-product-catalog-staging-disco.apps.fr01.paas.tech.orange/productCatalogManagement/v1/productOffering/6c4820dc-89ae-4c7a-adb0-23e9384407b4"
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
-  res.json(result);
+app.get('/processJson', async (req, res) => {
+  try {
+    const jsonData = await fetchJsonDataFromApi();
+
+    const result = {
+      id: jsonData.id,
+      name: jsonData.name,
+      bundledProductOffering: jsonData.bundledProductOffering.map(product => {
+        return {
+          id: product.id,
+          name: product.name
+        };
+      })
+    };
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
