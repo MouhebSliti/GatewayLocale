85938213b4db1afa2427cb343eb8203880f86481
Mouheb-Sliti-VR <mouheb.sliti@hotmail.fr>
2024-02-20 17:07:31 +0100

    update

--- routes/purchaseRoute.js
+++ routes/purchaseRoute.js
@@ -50,6 +50,7 @@ router.post('/buy', async (req, res) => {
 
 // POST request to handle the confirmConf action
 router.post('/confirmConf', async (req, res) => {
+    
     try {
         // Extract necessary data from the request body
         const { ID_ORANGE, username } = req.body;
@@ -96,6 +97,53 @@ router.post('/confirmConf', async (req, res) => {
         res.status(500).json({ error: 'Server Error' });
     }
 });
+
+router.post('/validateOrder', async (req, res) => {
+    
+    try {
+        // Extract necessary data from the request body
+        const { ID_ORANGE, username } = req.body;
+
+        // Construct the JSON payload
+        const payload = {
+            "relatedParty": [
+              {
+                "id": ID_ORANGE,
+                "name": username,
+                "role": "customer",
+                "@referredType": "individual"
+              }
+            ],
+            "characteristic": [
+              {
+                "name": "ValidateOrderByCustomer",
+                "valueType": "Object",
+                "value": {
+                  "productOrderId": "65d4caf69d6c4567d4b6da47",
+                  "orderValidationStatus": "orderValidatedByCustomer"
+                },
+                "@baseType": null,
+                "@schemaLocation": null,
+                "@type": "ObjectCharacteristic"
+              }
+            ]
+          };
+
+        // Make POST request to another backend endpoint
+        const response = await axios.post('https://clever-blue-bear.cyclic.app/mock/validateorder', payload);
+        // Log the success response
+        console.log('Success:', response.data);
+
+        // Send the data received from the external API in the response to the client
+        res.status(200).json(response.data);
+    } catch (error) {
+        // Log and handle errors
+        console.error('Error processing confirmConf request:', error);
+        res.status(500).json({ error: 'Server Error' });
+    }
+});
+
+
 
 module.exports = router;
 
