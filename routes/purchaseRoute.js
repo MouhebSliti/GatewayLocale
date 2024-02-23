ab007cebd9e1f96c17a6e97be1600f5a36bf8922
Mouheb-Sliti-VR <mouheb.sliti@hotmail.fr>
2024-02-23 14:08:50 +0100

    tessst

--- routes/purchaseRoute.js
+++ routes/purchaseRoute.js
@@ -220,11 +220,10 @@ router.post('/notification', async (req, res) => {
     
       console.log('Success:', response.data);
 
-      // Check if the state is "completed"
-    
+      if (response.data.state == "completed")    
           // Send a positive response indicating the buying is done
-          res.status(200).json({ message: 'Buying process completed successfully' });
-         
+          res.status(200).json(response.data); 
+  
   } catch (error) {
       // Log and handle errors
       console.error('Error processing the notification request:', error);
